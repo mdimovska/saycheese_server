568b3b9e03fb01af37ab7e54556a19b9e70bd64b
Milena Dimovska <dimovska.milena@gmail.com>
2014-09-07 12:37:46 +0200

    initial upload demo

--- server.js
+++ server.js
@@ -25,22 +25,10 @@ var fileupload = require('fileupload').createFileUpload('/uploadDir');
 
 
 http.createServer(function(req, res) {
-    /* Process the form uploads */
-    if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            res.writeHead(200, {'content-type': 'text/plain'});
-            res.write('received upload:\n\n');
-            res.end(util.inspect({fields: fields, files: files}));
-        });
-
-        return;
-    }
-
     /* Display the file upload form. */
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
-            '<form action="/upload" enctype="multipart/form-data" method="post">'+
+            '<form action="http://127.0.0.1:9000/upload" enctype="multipart/form-data" method="post">'+
             '<input type="text" name="title"><br>'+
             '<input type="file" name="upload" multiple="multiple"><br>'+
             '<input type="submit" value="Upload">'+
@@ -51,6 +39,17 @@ http.createServer(function(req, res) {
 
 app.get('/', function(req, res){
 	//next();?????
+});
+
+
+app.post("/upload",function(req,res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields: fields, files: files}));
+    });
+    return;
 });
 
 
